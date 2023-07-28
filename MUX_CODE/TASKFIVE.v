cfed710d2e2f338265ed04bd5fb4b83a28b0b9e4
Naveen M <115262661+Naveen8754@users.noreply.github.com>
2023-07-28 14:40:14 +0530

    Add files via upload

---
+++ MUX_CODE/TASKFIVE.v
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+
+
+//20E126 NAVEEN M//
+
+
+module TASKFIVE(
+    i,s,o);
+input[3:0]i;
+input[1:0]s;
+output o;
+assign o=((i[0]&~s[0]&~s[1])|(i[1]&~s[0]&s[1])|(i[2]&s[0]&~s[1])|(i[3]&s[0]&s[1]));
+
+
+endmodule
---
+++ MUX_CODE/muxtest.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+
+//20E126 NAVEEN M//
+
+module muxtest;
+
+	// Inputs
+	reg [3:0] i;
+	reg [1:0] s;
+
+	// Outputs
+	wire o;
+
+	// Instantiate the Unit Under Test (UUT)
+	TASKFIVE uut (
+		.i(i), 
+		.s(s), 
+		.o(o)
+	);
+
+	initial begin
+		// Initialize Inputs
+		i = 0;
+		s = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;i[0]=1'b0;s[0]=1'b0;s[1]=1'b0;
+		#100;i[1]=1'b1;s[0]=1'b1;s[1]=1'b0;
+        #100;i[2]=1'b0;s[0]=1'b0;s[1]=1'b1;
+		  #100;i[3]=1'b1;s[0]=1'b1;s[1]=1'b1;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
