83591cb478e7be8721a90943d967f768966faeb0
Naveen M <115262661+Naveen8754@users.noreply.github.com>
2023-07-28 14:34:57 +0530

    Add files via upload

---
+++ FOURBIT_CODE/taskthree.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:09:15 07/26/2023 
+// Design Name: 
+// Module Name:    taskthree 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module taskthree
+ (input [3:0] a,b,
+    input cin,
+    output co,
+    output [3:0] s
+    );
+wire c1,c2,c3;
+fulladder fa0(a[0],b[0],cin,s[0],c1);
+fulladder fa1(a[1],b[1],c1,s[1],c2);
+fulladder fa2(a[2],b[2],c2,s[2],c3);
+fulladder fa3(a[3],b[3],c3,s[3],co);
+endmodule
+
+module fulladder(input A,B,C,output sum,carry );
+wire d0,b0,b1;
+halfadder g1(A,B,d0,b0);
+halfadder g2(d0,C,carry,b1);
+or g3(sum,b1,b0);
+endmodule
+
+module halfadder(a,b,s,c);
+input a,b;
+output s,c;
+//assign{c,s} =a+b;
+assign s=a^b;
+assign c=a&b;
+endmodule
+
---
+++ FOURBIT_CODE/threetask03.v
@@ -0,0 +1,65 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   15:11:04 07/26/2023
+// Design Name:   taskthree
+// Module Name:   D:/20e126/fourbi/threetask03.v
+// Project Name:  fourbi
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: taskthree
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module threetask03;
+
+	// Inputs
+	reg [3:0] a;
+	reg [3:0] b;
+	reg cin;
+
+	// Outputs
+	wire co;
+	wire [3:0] s;
+
+	// Instantiate the Unit Under Test (UUT)
+	taskthree uut (
+		.a(a), 
+		.b(b), 
+		.cin(cin), 
+		.co(co), 
+		.s(s)
+	);
+
+	initial begin
+		// Initialize Inputs
+		a = 0;
+		b = 0;
+		cin = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100; a=4'b0001;b=4'b0011;cin=1'b0;
+      #100 a=4'b0101;b=4'b1011;cin=1'b0;
+      #100 a=4'b1101;b=4'b0011;cin=1'b1;
+      #100 a=4'b0011;b=4'b0011;cin=1'b0;
+      #100 a=4'b1001;b=4'b0011;cin=1'b1;
+      #100 a=4'b0001;b=4'b1011;cin=1'b0;
+      #100 a=4'b1111;b=4'b1101;cin=1'b1;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
