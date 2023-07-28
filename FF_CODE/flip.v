d8634de6060dcb8611fe836e06f0735cf6fd8a2a
Naveen M <115262661+Naveen8754@users.noreply.github.com>
2023-07-28 14:34:35 +0530

    Add files via upload

---
+++ FF_CODE/flip.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+
+//20E126 NAVEEN M//
+
+module flip;
+
+	// Inputs
+	reg a;
+	reg clk;
+
+	// Outputs
+	wire b;
+	wire c;
+
+	// Instantiate the Unit Under Test (UUT)
+	tasksix uut (
+		.a(a), 
+		.clk(clk), 
+		.b(b), 
+		.c(c)
+	);
+
+	initial begin
+		// Initialize Inputs
+		a = 0;
+		clk = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;a=1'b0;clk=1'b0;
+       #100;a=1'b1;clk=1'b1; 
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
---
+++ FF_CODE/tasksix.v
@@ -0,0 +1,18 @@
+`timescale 1ns / 1ps
+
+
+//20E126 NAVEEN M//
+
+
+module tasksix(
+   a,clk,b,c);
+input a,clk;
+output b,c;
+reg b,c;
+always @*
+begin 
+b=a;
+c=~a;
+end
+
+endmodule
