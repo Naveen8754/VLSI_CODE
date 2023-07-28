aac026b253ed155bb52943a1d6a25018f60a34d9
Naveen M <115262661+Naveen8754@users.noreply.github.com>
2023-07-28 15:11:16 +0530

    Add files via upload

---
+++ fourbit_comparator/fourbirtest.v
@@ -0,0 +1,60 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   15:07:25 07/28/2023
+// Design Name:   fourbitmag
+// Module Name:   D:/20e126/four_bi_magnitide/fourbirtest.v
+// Project Name:  four_bi_magnitide
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: fourbitmag
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module fourbirtest;
+
+	// Inputs
+	reg [3:0] A;
+	reg [3:0] B;
+
+	// Outputs
+	wire less;
+	wire equal;
+	wire greater;
+
+	// Instantiate the Unit Under Test (UUT)
+	fourbitmag uut (
+		.A(A), 
+		.B(B), 
+		.less(less), 
+		.equal(equal), 
+		.greater(greater)
+	);
+
+	initial begin
+		// Initialize Inputs
+		A = 0;
+		B = 0;
+
+		// Wait 100 ns for global reset to finish
+		 #100;A=4'b0001;B=4'b0100;
+        #100;A=4'b0101;B=4'b0100;
+		  #100;A=4'b0001;B=4'b1100;
+		  #100;A=4'b0011;B=4'b0100;
+		// Add stimulus here
+
+	end
+      
+endmodule
+
---
+++ fourbit_comparator/fourbitmag.v
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:05:14 07/28/2023 
+// Design Name: 
+// Module Name:    fourbitmag 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module fourbitmag(
+ A,B,  less, equal,greater);
+    input [3:0] A;
+    input [3:0]B;
+    output less;
+     output equal;
+     output greater;
+     reg less;
+     reg equal;
+     reg greater;
+    always @(A or B)
+     begin
+        if(A > B)  
+		  begin  
+            less = 0;
+            equal = 0;
+            greater = 1;  
+				end
+        else if(A == B)
+		  begin 
+            less = 0;
+            equal = 1;
+            greater = 0;  
+				end
+        else   
+		  begin 
+            less = 1;
+            equal = 0;
+            greater =0;
+        end 
+    end
+
+endmodule
