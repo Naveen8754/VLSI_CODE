54a6e8dd29fd915460caf4fc8b822440706da52e
Naveen M <115262661+Naveen8754@users.noreply.github.com>
2023-07-28 14:35:19 +0530

    Add files via upload

---
+++ fourbit_mag01/FOURMAG.v
@@ -0,0 +1,55 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    13:53:15 07/28/2023 
+// Design Name: 
+// Module Name:    FOURMAG 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module FOURMAG(
+    a,b,  c, d,e);
+	 
+	 
+    input [3:0] a;
+    input [3:0]b;
+    output c;
+     output d;
+     output e;
+     reg c;
+     reg d;
+     reg e;
+    always @(a or b)
+     begin
+        if(a > b)  
+		  begin  
+            c = 0;
+            d = 0;
+            e = 1;  
+				end
+        else if(a == b)
+		  begin 
+            c = 0;
+            d = 1;
+            e = 0;  
+				end
+        else   
+		  begin 
+            less = 1;
+            equal = 0;
+            greater =0;
+        end 
+    end
+
+endmodule
---
+++ fourbit_mag01/fourbittest.v
@@ -0,0 +1,61 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   13:55:00 07/28/2023
+// Design Name:   FOURMAG
+// Module Name:   D:/20e126/FOURBIT_MAG/fourbittest.v
+// Project Name:  FOURBIT_MAG
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: FOURMAG
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module fourbittest;
+
+	// Inputs
+	reg [3:0] a;
+	reg [3:0] b;
+
+	// Outputs
+	wire c;
+	wire d;
+	wire e;
+
+	// Instantiate the Unit Under Test (UUT)
+	FOURMAG uut (
+		.a(a), 
+		.b(b), 
+		.c(c), 
+		.d(d), 
+		.e(e)
+	);
+
+	initial begin
+		// Initialize Inputs
+		a = 0;
+		b = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;a=4'b0001;b=4'b0100;
+        #100;a=4'b0101;b=4'b0100;
+		  #100;a=4'b0001;b=4'b1100;
+		  #100;a=4'b0011;b=4'b0100;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
