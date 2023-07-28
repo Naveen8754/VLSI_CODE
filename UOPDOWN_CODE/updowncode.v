924ac580ccf93948419ce1e5f2d68a37c447788a
Naveen M <115262661+Naveen8754@users.noreply.github.com>
2023-07-28 14:39:36 +0530

    Add files via upload

---
+++ UOPDOWN_CODE/updowncode.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+
+
+///20E126 NAVEEN M//
+
+
+module updowncode(
+    input clk,rst,up,
+output reg [7:0] c);
+
+
+always @(posedge clk, negedge rst) 
+begin
+if (!rst) begin
+c <= 2'b0;
+end
+else if (up) begin
+c <= c + 1;
+end
+else begin
+c<= c - 1;
+end
+end
+
+
+endmodule
---
+++ UOPDOWN_CODE/updowntest.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+
+
+
+
+///20E126 NAVEEN M//
+
+
+
+
+module updowntest;
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg up;
+
+	// Outputs
+	wire [7:0] c;
+
+	// Instantiate the Unit Under Test (UUT)
+	updowncode uut (
+		.clk(clk), 
+		.rst(rst), 
+		.up(up), 
+		.c(c)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		rst = 0;
+		up = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;rst=0;up=1;
+		#100;rst=1;up=0;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
