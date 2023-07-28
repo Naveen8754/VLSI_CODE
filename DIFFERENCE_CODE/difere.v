d77550805723628bf8d9095c1c26422dde6f6c57
Naveen M <115262661+Naveen8754@users.noreply.github.com>
2023-07-28 14:34:13 +0530

    Add files via upload

---
+++ DIFFERENCE_CODE/difere.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+
+//20E126 NAVEEN M//
+
+module difere;
+
+	// Inputs
+	reg [3:0] a;
+	reg [3:0] b;
+
+	// Outputs
+	wire [3:0] s;
+	wire cout;
+
+	// Instantiate the Unit Under Test (UUT)
+	taskfour uut (
+		.a(a), 
+		.b(b), 
+		.s(s), 
+		.cout(cout)
+	);
+
+	initial begin
+		// Initialize Inputs
+		a = 0;
+		b = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;a=4'b1000; b=4'b1000;
+		#100 a=4'b0100; b=4'b0100;
+		#100 a=4'b0010; b=4'b0010;
+		#100 a=4'b0001; b=4'b0001;
+      #100 a=4'b0011; b=4'b0101; 
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
---
+++ DIFFERENCE_CODE/taskfour.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+
+
+//20e126 NAVEEN M//
+
+
+module taskfour(
+a,b,s,cout);
+input [3:0]a;
+input [3:0]b;
+output [3:0]s;
+output cout;
+wire c1,c2,c3;
+fullsub fs0(a[0],~b[0],1'b1,s[0],c1);
+fullsub fs1(a[1],~b[1],c1,s[1],c2);
+fullsub fs2(a[2],~b[2],c2,s[2],c3);
+fullsub fs3(a[3],~b[3],c3,s[3],cout);
+endmodule
+
+
+module fullsub(x,y,z,d,s);
+input x,y,z;
+output d,s;
+wire a,b,c,df,e;
+xor x1(a,x,y);
+not n1(b,x);
+and a1(c,b,y);
+xor x2(d,a,z);
+not n2(df,a);
+and a2(e,df,z);
+or o1(s,e,c);
+endmodule
+
